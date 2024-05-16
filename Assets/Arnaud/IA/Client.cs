1ed2a3beebbbebe73995eb26276a04b08359971d
AranudSDV <a_scottodivettimo@stu-lisaa.com>
2024-05-17 00:03:08 +0200

    c
    c

--- Assets/Arnaud/IA/Client.cs
+++ Assets/Arnaud/IA/Client.cs
@@ -7,7 +7,7 @@ public class Client : MonoBehaviour
 {
     private NavMeshAgent client;
 
-    bool bClientArrive = false;
+    public bool bClientArrive = false;
 
     public int iNombreArmoireMax = 1;
     public int iNombreItemMax = 1;
@@ -23,60 +23,28 @@ public class Client : MonoBehaviour
 
     bool ClientFini = false;
 
+    public LayerMask ArmoireMask;
+
     void Start()
     {
         client = GetComponent<NavMeshAgent>();
+        ClientSpawn();
     }
 
    
     void Update()
     {
-        int iNombreArmoireChoisie = Random.Range(0,2);
-
-        for(int i = 0; i < iNombreArmoireChoisie; i++)
-        {
-            
-            int iArmoireChoose = Random.Range(1,iNombreArmoireMax);
-
-            switch(iArmoireChoose)
-            {
-                case 1:
-                aArmoireChoose = ArmoirePosition1;
-                break;
-
-                case 2:
-                aArmoireChoose = ArmoirePosition2;
-                break;
-
-                case 3:
-                aArmoireChoose = ArmoirePosition3;
-                break;
-
-                case 4:
-                aArmoireChoose = ArmoirePosition4;
-                break;
-
-            }
-
-            client.SetDestination(aArmoireChoose.position);
+    
     }
         
-
-
-    /*void ClientArrive()
-    {
-        bClientArrive = true;
-        
-    }
 
     void ClientSpawn()
     {
+        
         int iNombreArmoireChoisie = Random.Range(0,2);
 
-        for(int i = 0; i < iNombreArmoireChoisie; i++)
+        for(int i = 0; i < 2; i++)
         {
-
-
             
             int iArmoireChoose = Random.Range(1,iNombreArmoireMax);
 
@@ -98,22 +66,33 @@ public class Client : MonoBehaviour
                 aArmoireChoose = ArmoirePosition4;
                 break;
 
+                case 5:
+                aArmoireChoose = ArmoirePosition4;
+                break;
+
             }
 
-            client.SetDestination(aArmoireChoose.position);
+            NavMeshHit hit;
+        
             
-            while(ClientFini == false)
+            if(NavMesh.Raycast(transform.position, client.transform.TransformDirection(Vector3.forward), out hit, ArmoireMask))
             {
-                
-                
-                
-                ClientFini = true;
+                bClientArrive = true;
             }
             
+
+            
+            if(bClientArrive == false)
+            {
+                client.SetDestination(aArmoireChoose.position);
+
+
+                Debug.Log(" client arrive == false");
+
+            }
         }
-        */
-    }
     
+    }
 }
     
     
