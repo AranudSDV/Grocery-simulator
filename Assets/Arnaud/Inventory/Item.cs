5d0cd771985479d34f11d423f90d05d4030ded53
AranudSDV <a_scottodivettimo@stu-lisaa.com>
2024-06-07 17:42:19 +0200

    daz
    azd

--- Assets/Arnaud/Inventory/Item.cs
+++ Assets/Arnaud/Inventory/Item.cs
@@ -7,7 +7,6 @@ public class item : MonoBehaviour
 
     new public string name = "New Item";
     public Sprite icon = null;
-    public float BuyPrice = 0;
     public float SellPrice = 0;
     public GameObject ItemSansID;
     
