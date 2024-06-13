d235728eb059167b1363452e2f0fa7401ed67492
GabinGuillot <gabinguillot64340@gmail.com>
2024-06-13 23:45:22 +0200

    Fin sons ambiance

--- Assets/Gabbin/sound/footsteps.cs
+++ Assets/Gabbin/sound/footsteps.cs
@@ -11,23 +11,33 @@ public class footsteps : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-        {
 
-            if (Outside == true)
+        if (Outside == true)
+        {
+            if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
             {
                 footstepsSound.enabled = true;
             }
-
-            if (Inside == true)
-             {
-                footstepsSoundInside.enabled = true;
-             }
+            
         }
         else
         {
             footstepsSound.enabled = false;
+        }
+
+        if (Inside == true)
+        {
+            if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            {
+                footstepsSoundInside.enabled = true;
+            }
+            
+        }
+
+        else
+        {
             footstepsSoundInside.enabled = false;
         }
+
     }
 }
